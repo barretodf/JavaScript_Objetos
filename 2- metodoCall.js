fde362645d0f8f4b7de30a7fbba6f57045be357f
barretodf <edsb1981@gmail.com>
2023-04-02 23:51:38 -0300

    passando parâmetros para call()

--- 2- metodoCall.js
+++ 2- metodoCall.js
@@ -9,21 +9,34 @@
    
 //    exibeInfos.call(user)
 
-function User(nome, email) {
-    this.nome = nome
-    this.email = email
+// function User(nome, email) {
+//     this.nome = nome
+//     this.email = email
    
-    this.exibeInfos = function(){
-      console.log(this.nome, this.email)
+//     this.exibeInfos = function(){
+//       console.log(this.nome, this.email)
+//     }
+//    }
+   
+//    const newUser = new User('mariana', 'm@m.com')
+
+//    const outroUser = {
+//     nome: 'Rodrigo',
+//     email: 'r@r.com'
+//    }
+   
+//    newUser.exibeInfos() //mariana m@m.com
+//    newUser.exibeInfos.call(outroUser) //Rodrigo r@r.com
+
+function exibeMensagem(nome, email) {
+    console.log(`usuário: ${nome}, email ${email}`)
+   }
+    const user = {
+    nome: 'Mariana',
+    email: 'm@m.com',
+    executaFuncao: function(fn) {
+      fn.call(user, this.nome, this.email)
     }
    }
    
-   const newUser = new User('mariana', 'm@m.com')
-
-   const outroUser = {
-    nome: 'Rodrigo',
-    email: 'r@r.com'
-   }
-   
-   newUser.exibeInfos() //mariana m@m.com
-   newUser.exibeInfos.call(outroUser) //Rodrigo r@r.com
+   user.executaFuncao(exibeMensagem) //usuário: Mariana, email m@m.com
